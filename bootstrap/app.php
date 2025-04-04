34b03d41bbaf4b24ea10197f368e68709402ee23
adkoy <yelda.adkoy@yahoo.com>
2025-04-04 19:34:48 +0330

    exception handling

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,16 +4,44 @@ use App\Http\Middleware\HandleEmptyString;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->append(HandleEmptyString::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        $exceptions->renderable(function (Throwable $e, Request $request) {
+            if ($e instanceof \Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException) {
+                return response()->json([
+                    'message' => ['Method Not Allowed.'],
+                    'status' => false,
+                ], 405);
+            }
+
+            if ($e instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException) {
+                return response()->json([
+                    'message' => ['Resource Not Found.'],
+                    'status' => false,
+                ], 404);
+            }
+
+            if ($e instanceof \Symfony\Component\HttpKernel\Exception\HttpException) {
+                return response()->json([
+                    'message' => [$e->getMessage() ?: 'HTTP Error'],
+                    'status' => false,
+                ], $e->getStatusCode());
+            }
+
+            return response()->json([
+                'message' => [$e->getMessage() ?: 'Internal Server Error.'],
+                'status' => false,
+            ], 500);
+        });
     })->create();
